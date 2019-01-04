af8b1204b5bc0c461199fae289facbb6b1c5ec07
reinier <reinier.v.linschoten@gmail.com>
2019-01-04 13:49:06 +0100

    added startup file

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from src.modules.data import keys
+from src.modules.mqtt.MQTT import MQTT
+
+if __name__ == "__main__":
+    listener = MQTT(keys.MQTT_BROKER, keys.MQTT_USERNAME, keys.MQTT_PASSWORD)
